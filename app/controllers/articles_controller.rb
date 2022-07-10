f59b9b80ad6bf89e5810e461ce0b228fd69799b3
Maciej Fedor <fedor.maciej@gmail.com>
2022-07-10 15:31:51 +0200

    add error messages and flash messages display

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,13 +10,17 @@ class ArticlesController < ApplicationController
 
 
   def new
-
+    @article = Article.new
   end
 
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
-    @article.save
+   if @article.save
+    flash[:notice] = "Article was created successfully!"
     redirect_to @article
+   else
+    render 'new'
+   end
   end
 
 end
