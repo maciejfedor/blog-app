65590fbfc4ae0d53172401c327ca3ebe85c34573
Maciej Fedor <fedor.maciej@gmail.com>
2022-07-16 15:51:56 +0200

    add edit category feature for admin user

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,6 +13,20 @@ class CategoriesController < ApplicationController
     @category = Category.new
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:notice] = "Category name updated succesfully!"
+      redirect_to @category
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @category = Category.new(category_params)
     if @category.save
