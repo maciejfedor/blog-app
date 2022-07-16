8b2fe93aafca7206cb3c8671d1faeb4c964a32c7
Maciej Fedor <fedor.maciej@gmail.com>
2022-07-16 15:43:11 +0200

    update category show action

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,8 +3,8 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
+    @articles = @category.articles.paginate(page: params[:page], per_page: 3)
   end
-
   def index
     @categories = Category.paginate(page: params[:page], per_page: 5)
   end
