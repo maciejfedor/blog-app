c1d320f9e26b5c60e1b1c9750ab590e7c8842561
Maciej Fedor <fedor.maciej@gmail.com>
2022-07-16 15:00:20 +0200

    create article form update - add association with catagories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,3 +1,5 @@
+require 'debug'
+
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
   before_action :require_user, except: [:show, :index]
@@ -20,6 +22,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
+    debugger
     @article = Article.new(article_params)
     @article.user = current_user
    if @article.save
@@ -53,7 +56,7 @@ def set_article
 end
 
 def article_params
-  params.require(:article).permit(:title, :description)
+  params.require(:article).permit(:title, :description, category_ids: [])
 end
 
 def require_same_user
